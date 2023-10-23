d9e6d39a2214a8ca04c9adc41d964b96b0d2b7d5
Dextry <1252471868@qq.com>
2023-10-23 11:10:18 +0800

    update

--- plot/fig1/plot_fig1.py
+++ plot/fig1/plot_fig1.py
@@ -4,9 +4,9 @@ import seaborn as sns
 import numpy as np
 
 # Load data
-df1 = pd.read_csv('output/mnist_fedavg_noniid.csv')
-df2 = pd.read_csv('output/mnist_fedavg_iid.csv')
-df3 = pd.read_csv('output/mnist_kcenter_noniid.csv')
+df1 = pd.read_csv('output/fig1/mnist_fedavg_noniid.csv')
+df2 = pd.read_csv('output/fig1/mnist_fedavg_iid.csv')
+df3 = pd.read_csv('output/fig1/mnist_kcenter_noniid.csv')
 
 
 # Plot data using accuracy vs. round
@@ -15,14 +15,14 @@ sns.lineplot(data=df1, x='round', y='accuracy', label='FedAvg (Non-IID)', ax=ax)
 sns.lineplot(data=df2, x='round', y='accuracy', label='FedAvg (IID)', ax=ax)
 sns.lineplot(data=df3, x='round', y='accuracy', label='K-Center (Non-IID)', ax=ax)
 
-#ax.set_title('MNIST')
+ax.set_title('Wireless Communication, Select 1 out of 2 ')
 ax.set_xlabel('Communication Round')
 ax.set_ylabel('Testing Accuracy (%)')
 # turn on grid lines
 ax.grid(True)
 ax.set_ylim(60, 100)
-ax.set_xlim(0, 200)
+ax.set_xlim(0, 100)
 #plt.show()
 
 # save the figure as a PNG
-fig.savefig('output/fig_1.png')
+fig.savefig('output/fig1/fig_1.png')
